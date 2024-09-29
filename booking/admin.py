83750ec6bb0e745512254077b7e3ff5136a4e625
Sawada <105200073+SawadaAndzen@users.noreply.github.com>
2024-09-29 12:12:13 +0300

    UP1COMPLETE

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Library, Book, Booking, Author
 
-# Register your models here.
+
+admin.site.register(Library)
+admin.site.register(Author)
+admin.site.register(Book)
+admin.site.register(Booking)
