ef1524ee843d73d90e6567944360b177dbcba57d
Sawada <105200073+SawadaAndzen@users.noreply.github.com>
2024-10-05 16:33:22 +0300

    UP2-done-version1

--- booking/urls.py
+++ booking/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('books/', views.read_books_list, name='books_list'),
+    path('add-book/', views.add_book, name='add_book'),
 ]
--- booking/views.py
+++ booking/views.py
@@ -1,5 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Book
+from .forms import BookForm
 
 
 def index(request):
     return render(request, 'index.html')
+
+
+def read_books_list(request):
+    books = Book.objects.all()
+    
+    return render(request, 'books_list.html', {'books': books})
+
+
+def add_book(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        
+        if form.is_valid():
+            form.save()
+            
+            return redirect('books_list')
+        
+    else:
+        form = BookForm()
+
+    return render(request, 'add_book.html', {'form': form})
